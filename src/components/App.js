1e844f24c76592df504eeb6e64263d5099e829f6
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-11-07 00:20:56 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ const App = () => {
   return (
     <>
       <h1 data-ns-test="project-name">Gowing Minds </h1>
-      <p data-ns-test="project-decription">
-        It is an Business Application in which the both the clients one who has
-        idea about the start-up Business(with no money) and the other one who
-        has an investment to start a Business but no idea .So by this
-        application the idea hamster can Log-In and post there Business ideas
-        and in the same the investers can choose the liked ideas and the can
-        connect ech other and start the start-up. They can contact or connect by
-        providing google map OR with the address.
-      </p>
+      <h5 data-ns-test="project-decription">
+        It is an Business Application in which the both the clients one who
+        hasidea about the start-up Business(with no money) and the other one
+        whohas an investment to start a Business but no idea .So by this
+        application the idea hamster can Log-In and post there Business ideasand
+        in the same the investers can choose the liked ideas and the can connect
+        ech other and start the start-up. They can contact or connect
+        byproviding google map OR with the address.
+      </h5>
     </>
   );
 };
