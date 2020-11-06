4f142783285dfa66878033119d4566c87db9bba8
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-11-07 00:03:17 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Gowing Minds </h1>
+      <p data-ns-test="project-decription">
+        It is an Business Application in which the both the clients one who has
+        idea about the start-up Business(with no money) and the other one who
+        has an investment to start a Business but no idea .So by this
+        application the idea hamster can Log-In and post there Business ideas
+        and in the same the investers can choose the liked ideas and the can
+        connect ech other and start the start-up.
+      </p>
+    </>
+  );
+};
 
 export default App;
