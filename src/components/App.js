149ebc045c17350f07df512e2b7c826a6cf7d7e6
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-11-07 00:29:40 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <h1 data-ns-test="project-name">Gowing Minds </h1>
-      <h5 data-ns-test="project-decription">
+      <p data-ns-test="project-decription">
         It is an Business Application in which the both the clients one who
         hasidea about the start-up Business(with no money) and the other one
         whohas an investment to start a Business but no idea .So by this
@@ -13,7 +13,7 @@ const App = () => {
         in the same the investers can choose the liked ideas and the can connect
         ech other and start the start-up. They can contact or connect
         byproviding google map OR with the address.
-      </h5>
+      </p>
     </>
   );
 };
